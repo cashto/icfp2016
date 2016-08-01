a6aaa92b5ddff7a9f8b980a3d2faa325f7cd4eb2
cashto <cashto@gmail.com>
2016-08-01 13:33:12 -0700

    Check in empty solution file

---
+++ Solver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool isPrime(int x)
+        {
+            for (int i = 2; i * i <= x; ++i)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solver;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckPrimes()
+        {
+            Assert.IsTrue(Program.isPrime(7));
+            Assert.IsFalse(Program.isPrime(25));
+            Assert.IsFalse(Program.isPrime(42));
+            Assert.IsTrue(Program.isPrime(2));
+        }
+    }
+}
